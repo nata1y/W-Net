d1b84268137e4da768507aa646acc414b22e47e0
nata1y <oxi-flent@mail.ru>
2021-03-05 21:19:59 +0100

    trying

--- main.py
+++ main.py
@@ -1,3 +1,59 @@
+from torch import optim, nn
+
+from wnet import WNet
+import torch
+from torch.utils import data
+from PIL import Image
+import numpy as np
+import os
+from torchvision import datasets, transforms
+from pathlib import Path
+import matplotlib.pyplot as plt
+import PIL
+import numpy as np
+
+img_path = Path('./BSR/BSDS500/data/images/')
+
+
+def categorical_image(tensor):
+    print(tensor.shape)
+    categories = torch.argmax(torch.squeeze(tensor), 0).numpy()
+    return categories
+
 
 if __name__ == '__main__':
-    pass
+
+    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+    print(f"Using device: {device}")
+        
+    transform = transforms.Compose([transforms.Resize(255),
+                                    transforms.CenterCrop(224),
+                                    transforms.ToTensor()])
+
+    print(img_path)
+
+    dataset = datasets.ImageFolder(img_path, transform=transform)
+
+    dataloader = torch.utils.data.DataLoader(dataset, batch_size=1, shuffle=True)
+    wnet = WNet(3, 64)
+    wnet.to(device)
+    print(wnet)
+    wnet.load_state_dict(torch.load("model_2_only_reconstruct.pth", map_location=torch.device('cpu')))
+    max_epoch = 5
+    #optimizer = optim.SGD(wnet.parameters(), lr=0.01, momentum=0.9)
+    optimizer = optim.Adam(wnet.parameters())
+    optimizer2 = optim.Adam(wnet.parameters(), lr=0.001)
+    
+    for epoch in range(max_epoch):
+        for batch_idx, (image, labels) in enumerate(dataloader):
+            image = image.to(device)
+            out1, out2, loss = wnet.train(image, optimizer, optimizer2)
+            print(f"Image: {batch_idx}, Loss: {loss * image.nelement()}")
+            #quit()
+            if (batch_idx % 100 == 99):
+                f, axarr = plt.subplots(1, 3)
+                axarr[0].imshow(transforms.ToPILImage()(torch.squeeze(image.cpu())))
+                axarr[1].imshow(categorical_image(out1.cpu()))
+                axarr[2].imshow(transforms.ToPILImage()(torch.squeeze(out2.cpu())))
+                plt.show()
+        torch.save(wnet.state_dict(), f"model_{epoch}_finetuning.pth")
--- module.py
+++ module.py
@@ -5,22 +5,37 @@ import torch.nn as nn
 
 class Module(nn.Module):
 
-    def __init__(self, in_channels, out_channels, n_features, id):
+    def __init__(self, in_channels, out_channels, separable=True):
         super(Module, self).__init__()
 
-        self.id = id
+        self.separable = separable
         self.in_channels = in_channels
         self.out_channels = out_channels
-        self.n_features = n_features
-        self.layer = []
-
-        for i in range(2, 6):
-            self.modules[i] = []
-
+        self.block = None
+        
+        if self.separable:
             # separable convolution 3*3
-            for conv in range(2):
-                kernels_per_layer = 0
-                depthwise_conv = nn.Conv2d(in_channels, in_channels * kernels_per_layer, kernel_size=3, padding=1, groups=in_channels)
-                pointwise_conv = nn.Conv2d(in_channels * kernels_per_layer, out_channels, kernel_size=1)
-                self.layers.append([depthwise_conv, pointwise_conv, nn.ReLU()])
-
+            self.block = nn.Sequential(
+                nn.Conv2d(in_channels, in_channels, kernel_size=3, padding=1, groups=in_channels),
+                nn.Conv2d(in_channels, out_channels, kernel_size=1),
+                nn.ReLU(),
+                nn.BatchNorm2d(out_channels),
+                #self.layers.extend([depthwise_conv, nn.ReLU(), pointwise_conv, nn.ReLU()])
+                nn.Conv2d(out_channels, out_channels, kernel_size=3, padding=1, groups=out_channels),
+                nn.Conv2d(out_channels, out_channels, kernel_size=1),
+                nn.ReLU(),
+                nn.BatchNorm2d(out_channels)
+                #self.layers.extend([depthwise_conv2, nn.ReLU(), pointwise_conv2, nn.ReLU()])
+            )
+        else:
+            self.block = nn.Sequential(
+                nn.Conv2d(in_channels, out_channels, kernel_size=3, padding=1),
+                nn.ReLU(),
+                nn.BatchNorm2d(out_channels),
+                nn.Conv2d(out_channels, out_channels, kernel_size=3, padding=1),
+                nn.ReLU(),
+                nn.BatchNorm2d(out_channels)
+            )
+                
+    def forward(self, x):
+        return self.block.forward(x)
