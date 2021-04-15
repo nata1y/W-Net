7acb95e74c835ec848015f4d57b4875949ad9972
Natalia <oxi-flent@mail.ru>
2021-04-15 13:16:17 +0200

    clean up

--- main.py
+++ main.py
@@ -1,18 +1,14 @@
-from torch import optim, nn
+from torch import optim
 
 from wnet import WNet
 import torch
 from torch.utils import data
-from PIL import Image
 import numpy as np
-import os
 from torchvision import datasets, transforms
 from pathlib import Path
-import matplotlib.pyplot as plt
-import sys
-import PIL
 
-img_path = Path('./BSR/BSDS500/data/images/')
+img_path = Path('./BSR/BSDS500/data/images/trainset/')
+test_path = Path('./BSR/BSDS500/data/images/testset/')
 
 
 def categorical_image(tensor, image, red_lines):
@@ -22,13 +18,13 @@ def categorical_image(tensor, image, red_lines):
     for x in range(np.max(categories) + 1):
         matches = categories == x
         if matches.any():
-            image_colors = np.stack((image[0, 0][matches],image[0, 1][matches],image[0, 2][matches]))
+            image_colors = np.stack((image[0, 0][matches], image[0, 1][matches], image[0, 2][matches]))
             color = np.mean(image_colors, axis=1)
             newImage[matches] = color
     if red_lines:
-        img_sobel = np.roll(categories,1, axis=0)!=categories
+        img_sobel = np.roll(categories, 1, axis=0) != categories
         newImage[img_sobel] = [1, 0, 0]
-        img_sobel = np.roll(categories,1, axis=1)!=categories
+        img_sobel = np.roll(categories, 1, axis=1) != categories
         newImage[img_sobel] = [1, 0, 0]
     return newImage
 
@@ -36,36 +32,37 @@ def categorical_image(tensor, image, red_lines):
 if __name__ == '__main__':
     device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
     print(f"Using device: {device}")
-        
+
     transform = transforms.Compose([transforms.Resize(255),
-                                    transforms.CenterCrop(112),
+                                    transforms.CenterCrop(224),
                                     transforms.ToTensor()])
 
     print(img_path)
 
     dataset = datasets.ImageFolder(img_path, transform=transform)
+    testdataset = datasets.ImageFolder(img_path, transform=transform)
 
     dataloader = torch.utils.data.DataLoader(dataset, batch_size=1, shuffle=True)
-    wnet = WNet(3, 20)
+    testdataloader = torch.utils.data.DataLoader(dataset, batch_size=1, shuffle=True)
+
+    # the second parameter is k, can be modified freely
+    wnet = WNet(3, 5)
     wnet.to(device)
     print(wnet)
-    # wnet.load_state_dict(torch.load("model_2_only_reconstruct.pth", map_location=torch.device('cpu')))
-    max_epoch = 5
-    #optimizer = optim.SGD(wnet.parameters(), lr=0.01, momentum=0.9)
+    max_epoch = 100
     optimizer = optim.Adam(wnet.parameters())
     optimizer2 = optim.Adam(wnet.parameters(), lr=0.001)
-    
+
     for epoch in range(max_epoch):
         for batch_idx, (image, labels) in enumerate(dataloader):
             image = image.to(device)
             out1, outcrf, out2, loss = wnet.train(image, optimizer, optimizer2, batch_idx % 500 == 0)
-            print(f"Image: {batch_idx}, Loss: {loss * image.nelement()}")
-            #quit()
-            if (batch_idx % 500 == 0):
-                f, axarr = plt.subplots(1, 4)
-                axarr[0].imshow(transforms.ToPILImage()(torch.squeeze(image.cpu())))
-                axarr[1].imshow(categorical_image(out1.cpu(), image.cpu(), True))
-                axarr[2].imshow(categorical_image(outcrf.cpu(), image.cpu(), True))
-                axarr[3].imshow(transforms.ToPILImage()(torch.squeeze(out2.cpu())))
-                plt.show()
-        torch.save(wnet.state_dict(), f"model_{epoch}_k=20.pth")
+        lozz = 0
+        ncutlozz = 0
+        for batch_idx, (image, labels) in enumerate(testdataloader):
+            image = image.to(device)
+            ncutloss, loss = wnet.test(image)
+            lozz += loss.detach()
+            ncutlozz += ncutloss.detach()
+        print(f"NCutLoss: {ncutlozz}, Loss: {lozz}")
+        torch.save(wnet.state_dict(), f"model_{epoch}_both_k=5.pth")
--- wnet.py
+++ wnet.py
@@ -15,7 +15,6 @@ class EncoderDecoder(nn.Module):
         super(EncoderDecoder, self).__init__()
 
         self.n_features = n_features
-        # self.enc1 = Module(in_channels=in_channels, out_channels=64, separable=False)
         self.up_convs = {}
 
         self.add_module("enc0", Module(in_channels=in_channels, out_channels=64, separable=False))
@@ -34,19 +33,13 @@ class EncoderDecoder(nn.Module):
 
         # up sampling
         for i in range(self.depth)[::-1]:
-            # self.modules[i] = Module(in_channels=(in_channels * 2), out_channels=in_channels, separable=(i!=9))
             self.add_module(f"up_conv2x2_{i}",
                             nn.ConvTranspose2d(in_channels=(in_channels * 2), out_channels=in_channels, kernel_size=2,
                                                stride=2))
 
             self.add_module(f"dec{i}",
                             Module(in_channels=(in_channels * 2), out_channels=in_channels, separable=(i != 0)))
-            # self.up_convs[i] = nn.ConvTranspose2d(in_channels=(in_channels*2), out_channels=in_channels,
-            #                                      kernel_size=2, stride=2)
             in_channels //= 2
-
-        # stride, padding?
-        # self.module_down_conn = nn.MaxPool2d(2)
 
         # placeholder until know amount of classes k?
         self.add_module("conv1x1", nn.Conv2d(in_channels * 2, n_features, 1))
@@ -83,7 +76,6 @@ def createKernel(sigma, r):
 kernel1d = createKernel(4, 5)
 kernel3d = torch.cat([kernel1d, kernel1d, kernel1d], 1)
 kernel3d = torch.cat([kernel3d, kernel3d, kernel3d], 0)
-# kernel3d = torch.cat([kernel1d, kernel1d, kernel1d], 0)
 kernel3d.requires_grad = False
 
 
@@ -118,46 +110,9 @@ def contour2ucm():
     pass
 
 
-def NCutLoss2D_Good(labels, inputs, num_features=224, sigma_x=4, sigma_i=10, r=5):
-    r"""Computes the continuous N-Cut loss, given a set of class probabilities (labels) and raw images (inputs).
-    Small modifications have been made here for efficiency -- specifically, we compute the pixel-wise weights
-    relative to the class-wide average, rather than for every individual pixel.
-    :param labels: Predicted class probabilities
-    :param inputs: Raw images
-    :return: Continuous N-Cut loss
-    """
-    num_classes = labels.shape[1]
-    #kernel = gaussian_kernel(radius=self.radius, sigma=self.sigma_1, device=labels.device.type)
-    loss = 0
-
-    for k in range(num_classes):
-        # Compute the average pixel value for this class, and the difference from each pixel
-        class_probs = labels[:, k].unsqueeze(1)
-        class_mean = torch.mean(inputs * class_probs, dim=(2, 3), keepdim=True) / \
-            torch.add(torch.mean(class_probs, dim=(2, 3), keepdim=True), 1e-5)
-        diff = (inputs - class_mean).pow(2).sum(dim=1).unsqueeze(1)
-
-        # Weight the loss by the difference from the class average.
-        weights = torch.exp(diff.pow(2).mul(-1 / sigma_i ** 2))
-
-        # Compute N-cut loss, using the computed weights matrix, and a Gaussian spatial filter
-        numerator = torch.sum(class_probs * F.conv2d(class_probs * weights, kernel1d, padding=r))
-        denominator = torch.sum(class_probs * F.conv2d(weights, kernel1d, padding=r))
-        loss += nn.L1Loss()(numerator / torch.add(denominator, 1e-6), torch.zeros_like(numerator))
-
-    return num_classes - loss
-
-
 def NCutLoss2D(labels, inputs, num_features=224, sigma_x=4, sigma_i=10, r=5):
     num_classes = labels.shape[1]
     loss = 0
-    # print(inputs.shape)
-    # print(labels.shape)
-
-    # this should give us an image with 3 channels
-    # where each channel is added according to a gaussian blur
-    # blocks = torch.nn.functional.unfold(inputs, (2*r+1, 2*r+1), padding=r)
-    # blocks = blocks.reshape((2*r+1)*(2*r+1), 3, 224, 224)
 
     weights = torch.nn.functional.conv2d(inputs, weight=kernel3d, padding=r)
     weights.requires_grad = False
@@ -168,39 +123,15 @@ def NCutLoss2D(labels, inputs, num_features=224, sigma_x=4, sigma_i=10, r=5):
 
     for k in range(num_classes):
         class_probs = labels[:, k]
-
-        # diff_pixel_values = torch.nn.functional.pairwise_distance(class_probs.reshape(class_probs.shape[1],
-        #                                                                     class_probs.shape[2], 1),
-        #                                                 class_probs.reshape(class_probs.shape[1],
-        #                                                                     class_probs.shape[2], 1))
-        diff_pixel_values = torch.cdist(class_probs.reshape(class_probs.shape[1],
-                                                                            class_probs.shape[2], 1),
-                                                        class_probs.reshape(class_probs.shape[1],
-                                                                            class_probs.shape[2], 1))
-
-        weights2 = torch.exp(diff_pixel_values.pow(2).mul(-1 / sigma_i ** 2))
-        result = torch.zeros(class_probs.shape)
-        result2 = torch.zeros(class_probs.shape)
-        print(result.shape)
-        for i in range(112):
-            for j in range(112):
-                result[0, i, j] = class_probs[0, i, j] * weights2[i, i, j]
-                result2[0, i, j] = weights2[i, i, j]
-        print(result.shape)
-        print(class_probs.shape)
-
-        result = result.unsqueeze(0)
-        result2 = result2.unsqueeze(0)
-        print(torch.nn.functional.conv2d(result, weight=kernel1d, padding=r).shape)
-        # Compute N-cut loss, using the computed weights matrix, and a Gaussian spatial filter
-        numerator = torch.sum(class_probs * torch.nn.functional.conv2d(result, weight=kernel1d, padding=r))
+        numerator = torch.sum(class_probs * torch.nn.functional.conv2d(
+            class_probs * weights2, weight=kernel1d, padding=r))
         # print("NUMERATOR ->", numerator)
-        denominator = torch.sum(class_probs * torch.nn.functional.conv2d(result2, weight=kernel1d, padding=r))
+        denominator = torch.sum(class_probs * torch.nn.functional.conv2d(
+            weights2, weight=kernel1d, padding=r))
         # print("DENOMINATOR ->", denominator)
         loss += nn.L1Loss()(numerator / torch.add(denominator, 1e-6), torch.zeros_like(numerator))
-        print("LOSS ->", loss)
+        # print("LOSS ->", loss)
 
-    print("LOSS:", num_classes - loss)
     return num_classes - loss
 
 
@@ -215,50 +146,32 @@ class WNet(nn.Module):
         self.U_dec = EncoderDecoder(n_features, 3)
         self.criterion = nn.MSELoss()
 
-    def soft_norm_cut_loss(self, output, image, k):
-        j = k - torch.sum()
-
     def forward(self, x):
         x = self.U_enc.forward(x)
         output_enc = self.soft_max.forward(x)
         output_dec = self.U_dec.forward(output_enc)
-        # output_dec = self.sigmoid.forward(output_dec)
         return output_dec
 
-    def train(self, image, optimizer, optimizer2, doCRF):
-        # output_enc = self.soft_max(self.U_enc.forward(image))
-        # n_cut_loss = gradient_regularization(output_enc)*0.5
-        # n_cut_loss.backward()
-        # ptimizer.step()
-        # ptimizer.zero_grad()
-        # print(image)
+    def test(self, image):
         x = self.U_enc.forward(image)
         output_enc = self.soft_max.forward(x)
-        # output_enc, output_dec = self.forward(image)
         ncutloss = NCutLoss2D(output_enc, image)
-        ncutloss.backward()
-        optimizer2.step()
-        optimizer2.zero_grad()
+        output_dec = self.forward(image)
+        loss = self.criterion(output_dec, image)
+        return ncutloss, loss
+
+    def train(self, image, optimizer, optimizer2, doCRF):
+        x = self.U_enc.forward(image)
+        output_enc = self.soft_max.forward(x)
+        # TODO: change type of loss here!
+        ncutloss = NCutLoss2D(output_enc, image)
         output_crf = None
+
         if doCRF:
             output_crf = crf(output_enc, image)
         output_dec = self.forward(image)
-        # print(image)
-        # print(NCutLoss2D(output_enc, image).item())
-        # print(image)
-        # print(output_dec)
-        # print(output_dec)
 
-        # loss = torch.mean(torch.pow(torch.pow(image, 2) + torch.pow(output_dec, 2), 0.5))*(1-0.5)
-        # loss = torch.pow(torch.norm(image - output_dec, p=2), 2)
-        # print(loss)
-
-        # print('Our loss: ', loss)
-        # print('Their loss: ', rec_loss)
-        # l1 = nn.L1Loss(reduction='sum')
         loss = self.criterion(output_dec, image)
-        # loss = torch.pow(loss, 2)
-        # print('And well known: ', loss)
         loss.backward()
         optimizer.step()
         optimizer.zero_grad()
